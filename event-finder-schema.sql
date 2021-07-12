aa9cb1881e1d7ad5cf7883544fcd866145e936e1
Sharon Sanchez <sharonsanchez581@gmail.com>
2021-07-12 10:38:00 -0700

    schema

---
+++ event-finder-schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users (
+    user_id        SERIAL,
+    first_name     TEXT NOT NULL,
+    last_name      TEXT NOT NULL,
+    password       TEXT NOT NULL,
+    email          TEXT NOT NULL UNIQUE CHECK (POSITION('@') IN email) > 1) 
+);
+
+CREATE TABLE event_organizers (
+    id              SERIAL,
+    organization    TEXT NOT NULL
+)
+
+CREATE TABLE events (
+    event_id          INTEGER NOT NULL,
+    event_name        TEXT NOT NULL,
+    event_organizer   TEXT NOT NULL,
+
+
+);
