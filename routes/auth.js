362d6f8484ee3750e95efe819069d5757dfcba27
Sharon Sanchez <sharonsanchez581@gmail.com>
2021-07-13 13:34:11 -0700

    fixed token bug

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ router.post("/login", async (req, res, next) => {
   try {
     const user = await User.login(req.body)
     const token = createUserJwt(user)
-    return res.status(200).json({ user, token  })
+    return res.status(200).json({ user })
   } catch (err) {
     next(err) 
   }
@@ -18,7 +18,7 @@ router.post("/register", async (req, res, next) => {
   try {
     const user = await User.register({ ...req.body})
     const token = createUserJwt(user)
-    return res.status(201).json({ user,token })
+    return res.status(201).json({ user })
   } catch (err) {
     next(err)
   }
