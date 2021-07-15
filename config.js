303adeb5e327d9bd6275c904476941aae4a22f95
Abbey311 <y.abbey2@gmail.com>
2021-07-15 14:09:33 -0400

    login and registration working on both ends

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 require("colors")
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret_dev"
-const PORT = process.env.PORT ? Number(process.env.PORT) : 3000
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3001
 
 const IS_TESTING = process.env.NODE_ENV === "test"
 
@@ -11,8 +11,7 @@ function getDatabaseUri() {
   const dbPass = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS) : "postgres"
   const dbHost = process.env.DATABASE_HOST || "localhost"
   const dbPort = process.env.DATABASE_PORT || 5432
-  const dbTestName = process.env.DATABASE_TEST_NAME || "life_tracker_test"
-  const dbProdName = process.env.DATABASE_NAME || "life_tracker"
+  const dbProdName = process.env.DATABASE_NAME || "event_finder"
   const dbName = process.env.NODE_ENV === "test" ? dbTestName : dbProdName
 
   return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
@@ -21,7 +20,7 @@ function getDatabaseUri() {
 // Speed up bcrypt for tests when security isn't important
 const BCRYPT_WORK_FACTOR = IS_TESTING ? 4 : 13
 
-console.log("Life Tracker Config:".red)
+console.log("Event Finder Config:".red)
 console.log("PORT:".blue, PORT)
 console.log("SECRET_KEY:".blue, SECRET_KEY)
 console.log("IS_TESTING:".blue, IS_TESTING)
