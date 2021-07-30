2bc4e63094ac5623dbc0d7a3cb2bd9f154d851c8
Abbey Yared <y.abbey2@gmail.com>
2021-07-30 14:24:34 -0400

    updated sql file

--- event-finder.sql
+++ event-finder.sql
@@ -7,12 +7,4 @@ CREATE DATABASE event_finder;
 
 \i event-finder-schema.sql
 
-
-\echo 'Delete and recreate event_finder_test db?'
-\prompt 'Return for yes or control-C to cancel > ' answer
-
-DROP DATABASE event_finder_test;
-CREATE DATABASE event_finder_test;
-\connect event_finder_test
-
-\i event-finder-schema.sql
+\i event-finder-data.sql
